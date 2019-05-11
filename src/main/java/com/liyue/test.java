0a86ba09759f7bfe2e8c098e2ab2771a02a4354b
Jack <Jack@163.com>
2019-05-11 21:02:26 +0800

    提交

--- src/main/java/com/liyue/test.java
+++ src/main/java/com/liyue/test.java
@@ -3,5 +3,7 @@ package com.liyue;
 public class test {
     public static void main(String[] args) {
         System.out.println("我爱你");
+        System.out.println("我爱你");
+        System.out.println("我爱你");
     }
 }
